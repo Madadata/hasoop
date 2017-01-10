283e561eca395afcd686e2cce64122bcd678a242
Jiayu Liu <jiayu@madadata.com>
2017-01-10 17:50:31 +0800

    adding coveralls

--- test/index.js
+++ test/index.js
@@ -1,8 +1,10 @@
 /* eslint-env mocha */
 import { expect } from 'chai'
+import index from '../src'
 
 describe('whatever should work', () => {
   it('works', () => {
     expect(1).to.be.equal(1)
+    expect(index).to.be.empty
   })
 })
