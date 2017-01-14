39eb25276efe099595a8373941bfff724a09fdca
Chyroc Chen <Chyroc@chenyunpengs-MacBook-Pro.local>
2017-01-15 01:04:52 +0800

    fix and use mysql docker for test

--- test/link.test.js
+++ test/link.test.js
@@ -11,21 +11,21 @@ suite('link', () => {
   test('getLinkAllForEmpty', async () => {
     const data = await sqoopClient.getLinkAll()
     expect(data).to.have.property('links')
-    expect(data['links']).to.have.lengthOf(0)
+    expect(data['links']).to.be.empty
   })
 
   test('createLinkForMysql', async () => {
     const config = {
       'linkName': 'test_link_1',
       'linkType': 'mysql',
-      'host': 'mysqlhost',
-      'databaseName': 'same',
+      'host': 'mysql',
+      'databaseName': 'harry',
       'username': 'root',
       'password': '1234'
     }
     const data = await sqoopClient.createLink(config)
-    console.log(JSON.stringify(data))
     expect(data['name']).to.equal('test_link_1')
+    expect(JSON.stringify(data['validation-result'])).to.equal('[{}]')
   })
 
   test('updateLinkForMysql and getLinkByLinkName', async () => {
@@ -33,13 +33,13 @@ suite('link', () => {
     const config = {
       'linkName': 'test_link_2',
       'linkType': 'mysql',
-      'host': 'mysqlhost',
-      'databaseName': 'same',
+      'host': 'mysql',
+      'databaseName': 'harry',
       'username': 'root',
       'password': '1234'
     }
     const updateData = await sqoopClient.updateLinkConfig(oldLinkName, config)
-    console.log(JSON.stringify(updateData))
+    expect(JSON.stringify(updateData['validation-result'])).to.equal('[{}]')
     const data = await sqoopClient.getLinkByLinkName('test_link_2')
     expect(data['links'][0]['name']).to.hasOwnProperty('test_link_2')
   }
@@ -55,14 +55,14 @@ suite('link', () => {
     const linkName = 'test_link_2'
     await sqoopClient.updateLinkDisable(linkName)
     const data = await sqoopClient.getLinkByLinkName(linkName)
-    expect(data['links'][0]['enabled']).to.equal(false)
+    expect(data['links'][0]['enabled']).to.be.false
   })
 
   test('updateLinkEnable', async () => {
     const linkName = 'test_link_2'
     await sqoopClient.updateLinkEnable(linkName)
     const data = await sqoopClient.getLinkByLinkName(linkName)
-    expect(data['links'][0]['enabled']).to.equal(true)
+    expect(data['links'][0]['enabled']).to.be.true
   })
 
   test('getLinkAll', async () => {
@@ -75,6 +75,6 @@ suite('link', () => {
     await sqoopClient.deleteLinkAll()
     const data = await sqoopClient.getLinkAll()
     expect(data).to.have.property('links')
-    expect(data['links']).to.have.lengthOf(0)
+    expect(data['links']).to.be.empty
   })
 })
