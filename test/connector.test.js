b01f405f3457bd95dbcf930e9df2c6015c506526
Chyroc Chen <yunpeng@madadata.com>
2017-02-12 17:43:29 +0800

    fix

--- test/connector.test.js
+++ test/connector.test.js
@@ -6,15 +6,10 @@ import { sqoopClient, expectSqoopHeaders } from './index'
 
 suite('connector', () => {
   test('getConnectorAll', async () => {
-    const resJson = await sqoopClient.getConnectorAll()
-      .then(res => {
-        expectSqoopHeaders(res)
-        return res.json()
-      })
-    expect(resJson.connectors.length).to.equal(7)
-    const connectorNames = _.map(resJson.connectors, 'name')
-    expect('hdfs-connector').to.be.oneOf(connectorNames)
-    expect('generic-jdbc-connector').to.be.oneOf(connectorNames)
+    const res = await sqoopClient.getConnectorAll()
+    const json = await res.json()
+    expectSqoopHeaders(res)
+    expect(json.connectors.length).to.equal(7)
   })
 
   test('getConnectorByConnectorName', async () => {
