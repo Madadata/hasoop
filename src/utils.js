78576c56c416c46d1568adfca5a639f2abcc16bf
Chyroc Chen <yunpeng@madadata.com>
2017-02-13 15:11:44 +0800

    refactor .toPairs and .fromPairs

--- src/utils.js
+++ src/utils.js
@@ -16,6 +16,14 @@ function splitTopConfig (config) {
   return _.omitBy(config, _.isObject)
 }
 
+const stringDecode = value => _.isString(value) ? decodeURIComponent(value) : value
+
+const keyCamelCaseValueDecode = data => _(data)
+  .toPairs()
+  .map(keyValue => [_.camelCase(keyValue[0]), stringDecode(keyValue[1])])
+  .fromPairs()
+  .value()
+
 /**
  * @ignore
  */
@@ -25,7 +33,7 @@ function splitInputsConfig (configs) {
     .flatten()
     .map(({name, value = null}) => {
       const nameSplit = name.split('.')
-      return {[nameSplit[1]]: _.isString(value) ? decodeURIComponent(value) : value}
+      return {[nameSplit[1]]: stringDecode(value)}
     })
     .value()
   return _.merge(...configListObject)
@@ -40,10 +48,10 @@ function splitInputsConfig (configs) {
 export function splitLinkConfig (linkInfo) {
   const topConfig = splitTopConfig(_.get(linkInfo, 'links[0]'))
   const otherConfig = splitInputsConfig(_.get(linkInfo, 'links[0].link-config-values.configs'))
-  return _.transform({
+  return keyCamelCaseValueDecode({
     ...topConfig,
     ...otherConfig
-  }, (result, value, key) => _.set(result, _.camelCase(key), _.isString(value) ? decodeURIComponent(value) : value), {})
+  })
 }
 
 /**
@@ -57,21 +65,16 @@ export function splitJobConfig (jobInfo) {
   const fromLinkConfig = splitInputsConfig(_.get(jobInfo, 'jobs[0].from-config-values.configs'))
   const toLinkConfig = splitInputsConfig(_.get(jobInfo, 'jobs[0].to-config-values.configs'))
   const driverConfig = splitInputsConfig(_.get(jobInfo, 'jobs[0].driver-config-values.configs'))
-  return _.transform({
+  return keyCamelCaseValueDecode({
     ..._.mapKeys(topConfig, (value, key) => `top_${key}`),
     ..._.mapKeys(driverConfig, (value, key) => `driver_${key}`),
     ..._.mapKeys(fromLinkConfig, (value, key) => `from_${key}`),
     ..._.mapKeys(toLinkConfig, (value, key) => `to_${key}`)
-  }, (result, value, key) => _.set(result, _.camelCase(key), _.isString(value) ? decodeURIComponent(value) : value), {})
+  })
 }
 
-export function splitSubmissionConfig (submissionInfo) {
-  const topConfig = _.transform({
-    ...splitTopConfig(_.get(submissionInfo, 'submissions[0]'))
-  }, (result, value, key) => _.set(result, _.camelCase(key), _.isString(value) ? decodeURIComponent(value) : value), {})
-  return {
-    topConfig,
-    fromSchemaConfig: _.get(submissionInfo, 'submissions[0].from-schema', {}),
-    toSchemaConfig: _.get(submissionInfo, 'submissions[0].to-schema', {})
-  }
-}
+export const splitSubmissionConfig = submissionInfo => ({
+  topConfig: keyCamelCaseValueDecode(splitTopConfig(_.get(submissionInfo, 'submissions[0]'))),
+  fromSchemaConfig: _.get(submissionInfo, 'submissions[0].from-schema', {}),
+  toSchemaConfig: _.get(submissionInfo, 'submissions[0].to-schema', {})
+})
